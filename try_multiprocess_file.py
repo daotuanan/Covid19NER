fb019286f99875dcbf70c43ebf8e154e7cc36257
daotuanan <daotuanan@gmail.com>
2020-09-09 11:51:34 +0000

    fix try multiprocess file

--- try_multiprocess_file.py
+++ try_multiprocess_file.py
@@ -10,6 +10,8 @@
 import multiprocessing as mp
 import os
 import json
+import functools
+import numpy
 # Import modules
 
 # Constants
@@ -18,14 +20,18 @@ import json
 
 def process(line):
     n = json.loads(line)
-    print(n['doc_id'])
+    return n['doc_id']
 
 def process_wrapper(input_file, chunkStart, chunkSize):
+    res = []
     with open(input_file,'r') as f:
         f.seek(chunkStart)
         lines = f.read(chunkSize).splitlines()
         for line in lines:
-            process(line)
+            tmp = process(line)
+            res.append(tmp)
+        print(res)
+    return res
 
 def chunkify(fname,size=1024*1024):
     fileEnd = os.path.getsize(fname)
@@ -39,21 +45,27 @@ def chunkify(fname,size=1024*1024):
             yield chunkStart, chunkEnd - chunkStart
             if chunkEnd > fileEnd:
                 break
+def numpy_flat(a):
+    return list(numpy.array(a).flat)
+
 
 def main():
     #init objects
-    pool = mp.Pool(64)
+    res = []
+    pool = mp.Pool()
     jobs = []
     input_file = './data/CORD-NER-ner.json' 
     #create jobs
-    for chunkStart,chunkSize in chunkify(input_file):
-        jobs.append( pool.apply_async(process_wrapper,(input_file,chunkStart,chunkSize)) )
-
+    tmp = [(input_file,chuckStart,chunkSize) for chuckStart, chunkSize in chunkify(input_file)]
+    
+    res = pool.starmap(process_wrapper,tmp)
+        
     #wait for all jobs to finish
-    for job in jobs:
-        job.get()
 
     #clean up
-    pool.close()
+    # pool.close()
+    # res2 = numpy_flat(res)
+    res2 = [item for sublist in res for item in sublist]
+    print(len(res2))
 if __name__ == '__main__':
     main()
